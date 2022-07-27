d8f28fddad26e81d81755a5fc4c46d7559571487
afaq-Khan-qbatch <afaq.khan@qbatch.com>
2022-07-27 12:28:21 +0500

    socket practice

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ const carts = require('./routes/carts');
 const checkPayment = require('./routes/checkPayment');
 const port = process.env.PORT || 3002;
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:3000',
+      method: ['Get', 'Post']
+    }
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +27,28 @@ app.get('/', (req , res) =>{
     res.send('ok');
 })
 
-app.listen(port , ()=>{
+http.listen(port , ()=>{
     console.log(`connection is running on port ${port}`)
 })
+
+io.on('connection', (socket) => {
+
+    console.log('new client connected: ', socket.id);
+    socket.emit('connection', null);
+
+    socket.on('send_msg', (data) => {
+        console.log({data});
+        //broad cast is use for ending message to all users
+        socket.broadcast.emit('recieve_Message', data );
+
+        //send data to specfic user
+
+        // socket.to(data.room).emit('recieve_Message', data);
+    })
+
+    socket.on('join_room', (data) => {
+        console.log('room => ',{data});
+        socket.join(data.room);
+    })
+
+});
